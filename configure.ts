ae3fc28037c0c8244c923b546dc5f4bf19f33c5f
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-03-13 09:34:42 +0100

    add: initial commit

---
+++ configure.ts
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------
+| Configure hook
+|--------------------------------------------------------------------------
+|
+| The configure hook is called when someone runs "node ace configure <package>"
+| command. You are free to perform any operations inside this function to
+| configure the package.
+|
+| To make things easier, you have access to the underlying "ConfigureCommand"
+| instance and you can use codemods to modify the source files.
+|
+*/
+
+import ConfigureCommand from '@adonisjs/core/commands/configure'
+
+export async function configure(_command: ConfigureCommand) {
+  const codemods = await _command.createCodemods()
+  await codemods.defineEnvValidations({
+    leadingComment: "Logsnag environment variables",
+    variables: {
+      LOGSNAG_TOKEN: 'Env.schema.string()',
+      LOGSNAG_PROJECT: 'Env.schema.string()'
+    }
+  })
+  await codemods.defineEnvVariables({ "LOGSNAG_TOKEN": "", "LOGSNAG_PROJECT": "" })
+
+}
