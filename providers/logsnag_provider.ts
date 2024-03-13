568d2c06d30c0b8c525c132fd0f8709ef9795fe0
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-03-13 11:10:59 +0100

    add: register logsnag in httpcontext

--- providers/logsnag_provider.ts
+++ providers/logsnag_provider.ts
@@ -1,4 +1,5 @@
 import { ApplicationService } from '@adonisjs/core/types'
+import { HttpContext } from '@adonisjs/core/http'
 import { LogsnagConfig } from '../src/types/main.js'
 import { LogSnag } from '@logsnag/node'
 
@@ -7,9 +8,17 @@ declare module '@adonisjs/core/types' {
     logsnag: LogSnag
   }
 }
+declare module '@adonisjs/core/http' {
+  export interface HttpContext {
+    logsnag: LogSnag
+  }
+}
 export default class LogsnagProvider {
   constructor(protected app: ApplicationService) {}
-
+  async boot() {
+    const logsnag = await this.app.container.make('logsnag')
+    HttpContext.getter('logsnag', () => logsnag)
+  }
   register() {
     this.app.container.singleton('logsnag', async () => {
       const config = this.app.config.get<LogsnagConfig>('logsnag', {})
